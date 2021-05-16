b523657f03624769c6217808784b2b58e40480d6
davidjbuchanan <davidjbuchanan@gmail.com>
2021-05-16 23:15:47 +0100

    functionality for making tasks as completed

--- todoapp/main/routes.py
+++ todoapp/main/routes.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template, redirect, url_for, request
+from bson.objectid import ObjectId
 from todoapp.extensions import mongo
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    todos_collection = mongo.db.todos
+    todos = todos_collection.find()
+    return render_template('index.html', todos = todos)
 
 
 @main.route('/add_todo', methods=['GET', 'POST'])
@@ -15,3 +18,12 @@ def add_todo():
     todos_collection = mongo.db.todos
     todos_collection.insert_one({'text' : todo_item, 'complete': False})
     return redirect(url_for('main.index'))
+
+
+@main.route('/complete_todo/<oid>')
+def complete_todo(oid):
+    todos_collection = mongo.db.todos
+    todo_item = todos_collection.find_one({'_id' : ObjectId(oid)})
+    todo_item['complete'] = True
+    todos_collection.save(todo_item)
+    return redirect( url_for('main.index'))
